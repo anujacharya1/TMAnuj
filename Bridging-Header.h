c69540695942682e291af42249be8b5b35c4f400
anujacharya <anujacharya1@gmail.com>
2015-03-28 00:58:14 -0700

    added bridging header for Facebook SDK integration with swift

---
+++ Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  Bridging-Header.h
+//  TMAnuj
+//
+//  Created by Anuj Acharya on 3/28/15.
+//  Copyright (c) 2015 Anuj Acharya. All rights reserved.
+//
+
+#ifndef TMAnuj_Bridging_Header_h
+#define TMAnuj_Bridging_Header_h
+
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+
+
+#endif
