feec137f4424988d1dec6f9bee31d44713af95d1
anujacharya <anujacharya1@gmail.com>
2015-03-27 23:52:35 -0700

    Added stadium image and zoom functionality

--- TMAnuj/ViewController.swift
+++ TMAnuj/ViewController.swift
@@ -8,13 +8,103 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UIScrollViewDelegate {
+    
+    @IBOutlet var scrollView: UIScrollView!
+    var imageView: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // 1
+        let image = UIImage(named: "beaverStadiumPNG.png")!
+        imageView = UIImageView(image: image)
+        imageView.frame = CGRect(origin: CGPoint(x: 0, y: 0), size:image.size)
+        scrollView.addSubview(imageView)
+        
+        // 2
+        scrollView.contentSize = image.size
+        
+        // 3
+        var doubleTapRecognizer = UITapGestureRecognizer(target: self, action: "scrollViewDoubleTapped:")
+        doubleTapRecognizer.numberOfTapsRequired = 2
+        doubleTapRecognizer.numberOfTouchesRequired = 1
+        scrollView.addGestureRecognizer(doubleTapRecognizer)
+        
+        // Use this for redirecting to the next ViewController
+        var singleTapRecognizer = UITapGestureRecognizer(target: self, action: "sectionView:")
+        singleTapRecognizer.numberOfTapsRequired = 2
+        singleTapRecognizer.numberOfTouchesRequired = 1
+        scrollView.addGestureRecognizer(singleTapRecognizer)
+
+        
+        // 4
+        let scrollViewFrame = scrollView.frame
+        let scaleWidth = scrollViewFrame.size.width / scrollView.contentSize.width
+        let scaleHeight = scrollViewFrame.size.height / scrollView.contentSize.height
+        let minScale = min(scaleWidth, scaleHeight);
+        scrollView.minimumZoomScale = minScale;
+        
+        // 5
+        scrollView.maximumZoomScale = 1.0
+        scrollView.zoomScale = minScale;
+        
+        // 6
+        centerScrollViewContents()
+    }
+    
+    func sectionView(recognizer: UITapGestureRecognizer){
+        // Use this to show the section view
+        // redirect to the new view controller to handle upload images
+    }
+    
+    func scrollViewDoubleTapped(recognizer: UITapGestureRecognizer) {
+        // 1
+        let pointInView = recognizer.locationInView(imageView)
+        
+        // 2
+        var newZoomScale = scrollView.zoomScale * 1.5
+        newZoomScale = min(newZoomScale, scrollView.maximumZoomScale)
+        
+        // 3
+        let scrollViewSize = scrollView.bounds.size
+        let w = scrollViewSize.width / newZoomScale
+        let h = scrollViewSize.height / newZoomScale
+        let x = pointInView.x - (w / 2.0)
+        let y = pointInView.y - (h / 2.0)
+        
+        let rectToZoomTo = CGRectMake(x, y, w, h);
+        
+        // 4
+        scrollView.zoomToRect(rectToZoomTo, animated: true)
     }
 
+    func centerScrollViewContents() {
+        let boundsSize = scrollView.bounds.size
+        var contentsFrame = imageView.frame
+        
+        if contentsFrame.size.width < boundsSize.width {
+            contentsFrame.origin.x = (boundsSize.width - contentsFrame.size.width) / 2.0
+        } else {
+            contentsFrame.origin.x = 0.0
+        }
+        
+        if contentsFrame.size.height < boundsSize.height {
+            contentsFrame.origin.y = (boundsSize.height - contentsFrame.size.height) / 2.0
+        } else {
+            contentsFrame.origin.y = 0.0
+        }
+        
+        imageView.frame = contentsFrame
+    }
+    
+    func viewForZoomingInScrollView(scrollView: UIScrollView!) -> UIView! {
+        return imageView
+    }
+    
+    func scrollViewDidZoom(scrollView: UIScrollView!) {
+        centerScrollViewContents()
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
